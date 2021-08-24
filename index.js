403cb207d4154e13c68c816ada1561f74341a7c5
block-hac <cn.corestar@gmail.com>
2021-08-24 21:53:25 +0800

    ssl

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const { Server } = require("socket.io");
 
 const router = require("./router");
@@ -14,6 +17,11 @@ const { startSaigonDaemon } = require("./daemons/vip/saigon");
 const { startSuperspeedDaemon } = require("./daemons/superspeed/superspeed");
 const socketEvents = require("./helpers/socketEvents");
 const conf = require("./config/main");
+
+const options = {
+  key: fs.readFileSync('/var/www/lotopoka/lotopoka.com.key'),
+  cert: fs.readFileSync('/var/www/lotopoka/lotopoka.com.crt')
+}
 
 mongoose
   .connect(process.env.DB_URL || conf.db_url, {
@@ -40,13 +48,6 @@ const corsOptions = {
   credentials: true,
 };
 
-const server = app.listen(conf.port, "0.0.0.0", () => {
-  console.log(`[SERVER]:[SUCCESS]:[${conf.port}]`);
-});
-
-const io = new Server(server, { cors: corsOptions });
-socketEvents(io);
-
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -66,13 +67,20 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use(cors(corsOptions));
-
 app.use(passport.initialize());
 app.use(passport.session());
 require("./config/passport")(passport);
 
 router(app);
+
+//const server = http.createServer(app);
+const server = https.createServer(options, app);
+const io = new Server(server, { cors: corsOptions });
+socketEvents(io);
+
+server.listen(conf.port, '0.0.0.0', function() {
+  console.log(`[SUCCESS]:[START]:[SERVER]:[AT]:[${conf.port}]]`);
+})
 
 startNorthernDaemon(io);
 startMegaDaemon(io);
